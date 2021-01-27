a449e569e4308b3583d9eabc84342544b0ed231e
EndMad <70961155+EndMad@users.noreply.github.com>
2021-01-27 12:06:13 +0300

    Update individual_1z_12var.py

--- individual_1z_12var.py
+++ individual_1z_12var.py
@@ -8,30 +8,32 @@
 # Покупатель приобрел 8 рулонов обоев по цене Х и две банки краски по цене Х.
 # Сколько он заплатил?
 
-x1 = int(input("Цена рулона обоев "))
-x2 = int(input("Цена банки краски "))
+if __name__ == '__main__':
 
-priobrel = (x1*8) + (x2*2)
-print("Цена без скидки ", priobrel)
+    x1 = int(input("Цена рулона обоев "))
+    x2 = int(input("Цена банки краски "))
 
-priobrel3 = priobrel - priobrel*0.03
-priobrel4 = priobrel - priobrel*0.05
-priobrel5 = priobrel - priobrel*0.07
-priobrel6 = priobrel - priobrel*0.09
+    priobrel = (x1*8) + (x2*2)
+    print("Цена без скидки ", priobrel)
+
+    priobrel3 = priobrel - priobrel*0.03
+    priobrel4 = priobrel - priobrel*0.05
+    priobrel5 = priobrel - priobrel*0.07
+    priobrel6 = priobrel - priobrel*0.09
 
 
-if 200 < priobrel < 500:
-    print("Ваша скидка 3%")
-    print("К оплате ", priobrel3)
-elif 500 < priobrel < 800:
-    print("Ваша скидка 5%")
-    print("К оплате ", priobrel4)
-elif 800 < priobrel < 1000:
-    print("Ваша скидка 7%")
-    print("К оплате ", priobrel5)
-elif priobrel > 1000:
-    print("Ваша скидка 9%")
-    print("К оплате ", priobrel6)
+    if 200 < priobrel < 500:
+        print("Ваша скидка 3%")
+        print("К оплате ", priobrel3)
+    elif 500 < priobrel < 800:
+        print("Ваша скидка 5%")
+        print("К оплате ", priobrel4)
+    elif 800 < priobrel < 1000:
+        print("Ваша скидка 7%")
+        print("К оплате ", priobrel5)
+    elif priobrel > 1000:
+        print("Ваша скидка 9%")
+        print("К оплате ", priobrel6)
 
 
 print("Спасибо за покупку!")
