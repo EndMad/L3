36bbab17ed8783b3a228ef5f55e7e8b042b7ef8b
EndMad <70961155+EndMad@users.noreply.github.com>
2021-02-04 22:15:29 +0300

    Update Pr_3.py

--- Pr_3.py
+++ Pr_3.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     n = int(input("Value of n? "))
     x = float(input("Value of x? "))
     S = 0.0
-
     for k in range(1, n + 1):
         a = math.log(k * x) / (k * k)
         S += a
